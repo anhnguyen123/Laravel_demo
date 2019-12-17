efc1e5849e3a10e551a18b304df03e3c32c739eb
NgocAnh <anhnguyen29989@gmail.com>
2019-12-17 15:27:36 +0700

    bài 26 đổ dữ liệu ra slide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\TheLoai;
+use App\Slide;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $theloai = TheLoai::all();
+        $slide = Slide::all();
+        view()->share('slide', $slide); 
         view()->share('theloai', $theloai); 
     }
 }
